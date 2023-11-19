7b1defc87634a74e577f563053b08faa71d02f58
Pedro Teixeira <pedrohcrt@gmail.com>
2023-11-19 19:25:48 -0300

    Feat: Implementação de navegações diversas

--- APPGrupo1/src/routes/index.jsx
+++ APPGrupo1/src/routes/index.jsx
@@ -1,28 +1,55 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from '../screens/Login';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import React from 'react';
 import Cadastro from '../screens/Cadastro';
+import Descricao from '../screens/Descricao';
 import Home from '../screens/Home';
-import Produtos from '../Screens/Produtos';
-import Descricao from '../Screens/Descricao';
-
-
-
+import Login from '../screens/Login';
+import Produtos from '../screens/Produtos';
 
 const Stack = createNativeStackNavigator();
+const Tab = createMaterialTopTabNavigator();
+const Drawer = createDrawerNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator
+      tabBarPosition='bottom'>
+      <Tab.Screen name='Home' component={Home} />
+      <Tab.Screen name='Produtos' component={Produtos} />
+      <Tab.Screen name='Cadastro' component={Cadastro} />
+    </Tab.Navigator>
+  )
+}
+
+function Stacks() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Login' component={Login} />
+      <Stack.Screen name='Home' component={Home} />
+    </Stack.Navigator>
+  )
+}
 
 export const Rotas = () => {
   return (
-      <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name="Produtos" component={Produtos} />
-        <Stack.Screen name="Descricao" component={Descricao} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Cadastro" component={Cadastro} />
-        <Stack.Screen name="Home" component={Home} />
-        
-      </Stack.Navigator>
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          headerShown: true
+        }}
+      >
+        <Drawer.Screen name='Tabs' component={Tabs}
+          options={{
+            drawerItemStyle: { height: 0 }
+          }} />
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Login" component={Login} />
+        <Drawer.Screen name="Cadastro" component={Cadastro} />
+        <Drawer.Screen name="Produtos" component={Produtos} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };

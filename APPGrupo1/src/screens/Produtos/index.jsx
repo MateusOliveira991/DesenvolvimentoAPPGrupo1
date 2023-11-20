45d8a0a983187c962c9e256e75189722a99ba79d
Pedro Teixeira <pedrohcrt@gmail.com>
2023-11-20 00:01:42 -0300

    Feat: página da lista de produtos criada

--- APPGrupo1/src/screens/Produtos/index.jsx
+++ APPGrupo1/src/screens/Produtos/index.jsx
@@ -1,44 +1,83 @@
-import React from "react";
-import { View, Image, TouchableOpacity, Text } from "react-native";
-import detetiveImg from "../../assets/detetive.jpg";
-import caraImg from "../../assets/cara.webp";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { Feather, EvilIcons } from "@expo/vector-icons"
 
-const Produtos = ({ navigation }) => {
-  const handleMostrarCara = () => {
-    navigation.navigate("Descricao", { imagem: caraImg, texto: "cara" });
-  };
+const Produtos = () => {
+  const [produtos, setProdutos] = useState()
 
-  const handleMostrarDetetive = () => {
-    navigation.navigate("Descricao", { imagem: detetiveImg, texto: "detetive" });
-  };
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    axios.get("https://655a8d516981238d054d8fe9.mockapi.io/g1/produtos")
+      .then(response => {
+        setProdutos(response.data)
+      }).catch(() => {
+        Alert.alert("Não foram encontrados produtos")
+        navigation.navigate("Home")
+      })
+  }, [])
+
+  function excluirProduto(id) {
+    axios.delete(`https://655a8d516981238d054d8fe9.mockapi.io/g1/produtos/${id}`)
+    setProdutos(produtos.filter(produto => produto.id !== id))
+  }
 
   return (
-    <View>
+    <View style={styles.container}>
+      <Text style={{ fontSize: 25, paddingVertical: 10 }}>Produtos:</Text>
       <View>
-        <Image source={caraImg} style={{ width: "100%", height: 500, marginTop: 90 }} />
+        <FlatList
+
+          data={produtos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.lista}>
+              <Text style={styles.titulo}>{item.nomeProduto}</Text>
+              <Image
+                source={{ uri: `../../assets/${item.nomeArquivo}` }}
+                style={styles.imagem}
+              />
+
+              <Text style={styles.descricao}>{item.descricao}</Text>
+              <Text style={styles.valor}>Valor: R${item.valor}</Text>
+
+              <TouchableOpacity onPress={() => excluirProduto(item)}>
+                <EvilIcons name="trash" size={18} color={"black"} />
+              </TouchableOpacity>
+            </View>
+          )}
+        />
       </View>
-
-      <Text>
-        Jogo Cara
-      </Text>
-
-      <TouchableOpacity onPress={handleMostrarCara}>
-        <Text>Descrição Cara</Text>
-      </TouchableOpacity>
-
-      <View>
-        <Image source={detetiveImg} style={{ width: '55%', height: 'auto', marginTop: 90 }} />
-      </View>
-
-      <Text>
-        Jogo Detetive
-      </Text>
-
-      <TouchableOpacity onPress={handleMostrarDetetive}>
-        <Text>Descrição Detetive</Text>
-      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  lista: {
+    paddingBottom: 20
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  imagem: {
+    width: 100,
+    height: 50
+  },
+  descricao: {
+    fontSize: 18
+
+  },
+  valor: {
+    fontSize: 18
+
+  }
+})
 
 export default Produtos;

9a6887ea8cb1471292de0606f6be791a0e63eecf
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2023-11-21 07:15:18 -0300

    Criação de menus de descrição na página inicial

--- APPGrupo1/src/screens/Descricao/index.jsx
+++ APPGrupo1/src/screens/Descricao/index.jsx
@@ -1,39 +1,81 @@
+
 import React, { useState } from "react";
 import { View, Image, TouchableOpacity, Text } from "react-native";
-import detetiveImg from "../../assets/detetive.jpg";
-import caraImg from "../../assets/cara.webp";
+import detetive from "../../assets/detetive.jpg";
+import cara from "../../assets/cara.webp";
+import jumanji from "../../assets/jumanji.webp";
+import zathura from "../../assets/zathura.jpg";
+import ouija from "../../assets/ouija.jpg";
 import { Ionicons } from '@expo/vector-icons';
+import { styles } from './style'
 
-const Descricao = ({ navigation, route }) => {
-  const [imagem, setImagem] = useState(route.params.imagem);
-  const [texto, setTexto] = useState(route.params.texto);
+  const Descricao = ({ navigation, route }) => {
+  const [imagem, setImagem] = useState(route.params?.imagem || detetive);
+  const [texto, setTexto] = useState(route.params?.texto || "Produtos:");
 
   const handleFechar = () => {
-    if (imagem === detetiveImg) {
-      setImagem(caraImg);
-      setTexto("cara");
-    } else {
-      setImagem(detetiveImg);
-      setTexto("detetive");
+    switch (imagem) {
+      case cara:
+        setImagem(jumanji);
+        setTexto("jumanji");
+        break;
+      case detetive:
+        setImagem(zathura);
+        setTexto("zathura");
+        break;
+      case jumanji:
+        setImagem(ouija);
+        setTexto("ouija");
+        break;
+      case zathura:
+        setImagem(cara);
+        setTexto("cara");
+        break;
+      case ouija:
+        setImagem(detetive);
+        setTexto("detetive");
+        break;
+      default:
+        setImagem(detetive);
+        setTexto("detetive");
+        break;
     }
-  };
-
+    };
+  
   return (
-    <View>
-      <Image source={imagem} style={{ width: '55%', height: '100%', marginTop: 90 }} />
+    <View style={styles.container}>
+      <Image source={imagem} style={styles.image} />
+      <Text style={styles.text}>{texto}</Text>
 
-      <Text>
-        {texto}
-      </Text>
-
-      <View>
-        <TouchableOpacity onPress={() => navigation.navigate("Produtos")}>
-          <Ionicons name="settings-sharp" size={24} color="black" onPress={handleFechar} />
-          <Text>Fechar</Text>
-        </TouchableOpacity>
-      </View>
+      {route.params ? (
+        <View>
+          <TouchableOpacity onPress={() => navigation.navigate("Descricao")}>
+            <Ionicons name="settings-sharp" size={24} color="black" onPress={handleFechar} />
+            <Text style={styles.iconText}>Fechar</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+       
+        <View>
+          <TouchableOpacity onPress={() => navigation.navigate("Descricao", { imagem: jumanji, texto: "jumanji" })}>
+            <Text style={styles.blueText}>Jumanji</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("Descricao", { imagem: zathura, texto: "zathura" })}>
+            <Text style={styles.blueText}>Zathura</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("Descricao", { imagem: ouija, texto: "ouija" })}>
+            <Text style={styles.blueText}>Ouija</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("Descricao", { imagem: cara, texto: "cara" })}>
+            <Text style={styles.blueText}>Cara-a-Cara</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("Descricao", { imagem: detetive, texto: "detetive" })}>
+            <Text style={styles.blueText}>Detetive</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 };
 
-export default Descricao;
+export default Descricao

c451c864caaa73eeb78a05c8367ae5131fa0d188
Thiago Bellato <141455937+thiagobellato@users.noreply.github.com>
2023-11-22 17:59:21 -0300

    Feat: Modal (Começo)

---
+++ APPGrupo1/src/components/Modal/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const estilos = StyleSheet.create({
+    modal: {
+        backgroundColor: "#000",
+        margin: 20,
+        padding: 20,
+        borderRadius: 20,
+        elevation: 10
+    },
+    textoModal: {
+        color: "#fff",
+        textAlign: 'center'
+    }
+
+});
+
+export default estilos;

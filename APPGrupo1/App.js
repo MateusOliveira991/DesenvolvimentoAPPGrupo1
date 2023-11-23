c9b7826177dd0954b5cc99e7bdcf347892d78a6b
Pedro Teixeira <pedrohcrt@gmail.com>
2023-11-23 01:40:10 -0300

    fix: removendo bug de modal

--- APPGrupo1/App.js
+++ APPGrupo1/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Rotas } from './src/routes';
 import ProdutosProvider from './src/context/ProdutosContext';
-import ModalProvider from './src/components/Modal';
 
 export default function App() {
   return (
     <ProdutosProvider>
-      <ModalProvider>
         <Rotas />
-      </ModalProvider>
     </ProdutosProvider>
   )
 }
